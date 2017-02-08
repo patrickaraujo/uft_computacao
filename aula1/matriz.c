1c421ec98dffcaceb81e56c6d72b5b6ee46485c7
patrickaraujo <araujo.patrick@live.com>
2017-02-08 16:25:05 -0200

    Update matriz.c

--- aula1/matriz.c
+++ aula1/matriz.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	Exercício em sala
+	- Programa em C para criar uma matriz com números aleatórios em inteiro, com uma função para alocar e outra para imprimir
+*/
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int **alocar(int linhas, int colunas){
 	int **array2d;
 	int i, j;
     int *gridstart;
-    array2d = malloc((linhas * sizeof(int *)) + (linhas * colunas * sizeof(int)));
+    array2d = malloc(linhas * sizeof(int *));										//	(linhas * sizeof(int *)): array de arrays 
+    																				//	https://www.youtube.com/watch?v=7nW0c1HMiXo
 	/* Demote to char for safe pointer arithmetic */
-    gridstart = (int *)((char *)array2d + (linhas * sizeof(int *)));
     // (int *), (char *): São tipos de cast
-    
     for ( i = 0; i < linhas; i++ ) {
-        array2d[i] = gridstart + i * colunas;
+        array2d[i] = malloc(colunas*sizeof(int));
         for ( j = 0; j < colunas; j++ ) 
             array2d[i][j] = rand() % 100;
     }
-
     return array2d;
-    
 }
 	
 void printtdArray(int **x, int linhas, int colunas){
@@ -41,6 +42,5 @@ int main(int argc, char *argv[]) {
 	//	int (*pointer)[linhas][colunas];
 	int **x = alocar(((sizeof(tdArray))/(sizeof(tdArray[0]))), ((sizeof(tdArray[0]))/(sizeof(tdArray[0][0]))));	// Guardar pra vida
 	printtdArray(x, ((sizeof(tdArray))/(sizeof(tdArray[0]))), ((sizeof(tdArray[0]))/(sizeof(tdArray[0][0]))));
-	
 	return 0;
 }
