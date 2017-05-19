36024503f30015d5b4037ee078e2aa3f5c60255b
patrickaraujo <araujo.patrick@live.com>
2017-05-18 22:06:28 -0300

    Create main.c

---
+++ AED1/P1/Ex1/main.c
@@ -0,0 +1,89 @@
+/**
+* @author Patrick Araújo
+* Lista Duplamente Encadeada em C para a aula de Algorismo e Estrutura de Dados 1
+*
+* Assumindo que os códigos para as funções Pilha_empilha(Pilha* P, int dado); e
+* Pilha_desempilha(Pilha* P, int &dado); estejam implementadas, faça um programa
+* em C que receba uma expressão matemática usando operadores simples e as marcações
+* de chaves, colchetes e parênteses e verifique se a equação está correta do ponto de
+* vista da abertura de chaves, parênteses e colchetes
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int x;
+}no;
+
+typedef struct{
+    no* inicio;
+    no* fim;
+    unsigned int qtde;
+}Lista;
+
+typedef Lista Pilha;
+
+Lista* create(){
+    Lista* nova = (Lista*)malloc(sizeof(Lista));
+    if(nova){
+        nova->qtde = 0;
+        nova->inicio = NULL;
+        nova->fim = NULL;
+    }
+    return nova;
+}
+
+// retorna 0 para erro ou 1 para sucesso
+Pilha_empilha(Pilha* P, int dado);
+
+// retorna 0 para erro ou 1 para sucesso
+Pilha_desempilha(Pilha* P, int &dado)
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
+
+int bemFormada (char s[]) {
+    int i;
+    criapilha ();
+
+    for (i = 0; s[i] != '\0'; ++i) {
+        char c;
+        switch (s[i]) {
+            case ')':
+                if (pilhavazia ())
+                    return 0;
+                c = desempilha ();
+                if (c != '(') return 0;
+                    break;
+            case ']':
+                if (pilhavazia ())
+                    return 0;
+                c = desempilha ();
+                if (c != '[') return 0;
+                    break;
+            default:
+                empilha (s[i]);
+        }
+    }
+    return pilhavazia ();
+}
+
+void criapilha (void) {
+   t = 0;
+}
+
+void empilha (char y) {
+   pilha[t++] = y;
+}
+
+char desempilha (void) {
+   return pilha[--t];
+}
+
+int pilhavazia (void) {
+   return t <= 0;
+}
