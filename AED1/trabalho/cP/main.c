8d56f038e767a03f14fc55c840a198969a46a7a6
patrickaraujo <araujo.patrick@live.com>
2017-06-04 16:51:36 -0300

    Update main.c

--- AED1/trabalho/cP/main.c
+++ AED1/trabalho/cP/main.c
@@ -5,29 +5,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
+#include <string.h>
+
+typedef struct pl{
+    char palavras[30];
+    struct pl *next;
+}Palavras;
 
 typedef struct reg{
     char info;
     struct reg *prox ;
 }Lista;
 
-typedef struct word{
-    char *info;
-    struct reg *prox ;
-}Palavras;
 
 void push(Lista **aux, Lista **prim, char letra);
 void imprimir(Lista *auxa, int i, int j);
-void pushWords(Palavras **aux, Palavras **prim, char *palavras);
+void pushWords(Palavras **auxP, Palavras **primP, char *palav);
+void procurarPalavras(Lista *prim, Palavras *primP, int i, int j, int k);
+void procurarDirecoes(char *palavra, Lista *prim, int i, int j);
 
 int main(){
+    Palavras *primPalavras;
+    Palavras *pPalavras = NULL;
+
+    Lista *prim;
+    Lista *p = NULL;
+
     FILE *fp;
     char buff;
     fp = fopen("C:\\Users\\Patrick\\Documents\\C\\cP\\File\\cP.txt", "r");
     int i = 0, j = 0;
-    Lista *prim;
-    Lista *p = NULL;
     char letra;
     do{
         letra = fgetc(fp);
@@ -44,6 +51,7 @@ int main(){
     }while(letra != EOF);
     i++;
     j++;
+    printf("i: %i\nj: %i", i, j);
     imprimir(prim, i, j);
     fclose(fp);
 
@@ -51,8 +59,7 @@ int main(){
     w = fopen("C:\\Users\\Patrick\\Documents\\C\\cP\\File\\palavras.txt", "r");
     char words[30];
     int k = 0;
-    Palavras *primPalavras;
-    Palavras *pPalavras = NULL;
+
     printf("\nPalavras: \n");
     while(fgets(words, 30, w)) {
         printf("%s\n", words);
@@ -61,8 +68,24 @@ int main(){
     }
     fclose(w);
 
+    procurarPalavras(prim, primPalavras, i, j, k);
 
     return 0;
+}
+
+void pushWords(Palavras **auxP, Palavras **primP, char *palav) {
+    Palavras *plv;   //  auxiliar
+    if((plv = malloc(sizeof(Palavras))) == NULL)	//	erro
+        printf("\nMemory Failure");
+    else{
+        strcpy(plv->palavras, palav); //recebendo valor
+        plv->next = NULL;	//	esquerda é nulo pois a inseção é no inicio
+        if(*auxP != NULL)    //  se a lista não for nula então a esquerda recebe o valor
+            (*auxP)->next = plv;
+        else
+            (*primP) = plv;
+        (*auxP) = plv; //lista recebe valor da direita
+    }
 }
 
 void push(Lista **aux, Lista **prim, char letra) {
@@ -80,28 +103,29 @@ void push(Lista **aux, Lista **prim, char letra) {
     }
 }
 
-void pushWords(Palavras **aux, Palavras **prim, char *palavras) {
-    Palavras *p;   //  auxiliar
-    if((p = malloc(sizeof(Palavras))) == NULL)	//	erro
-        printf("\nMemory Failure");
-    else{
-        p->info = palavras; //recebendo valor
-        p->prox = NULL;	//	esquerda é nulo pois a inseção é no inicio
-        if(*aux != NULL)    //  se a lista não for nula então a esquerda recebe o valor
-            (*aux)->prox = p;
-        else
-            (*prim) = p;
-        (*aux) = p; //lista recebe valor da direita
+void procurarPalavras(Lista *prim, Palavras *primP, int i, int j, int k){
+    int l = 0;
+    for(l = 0; l < k; l++){
+        procurarDirecoes(primP->palavras, prim, i, j);
+        primP = primP->next;
     }
 }
 
-void procurarPalavras(Lista *prim, Palavras *primP, int i, int j, int k){
-    int l = 0, m = 0, n = 0;
-    for(l = 0)
+void procurarDirecoes(char *palavra, Lista *prim, int i, int j){
+    printf("palavra: %s", palavra);
+    pSVH(prim, palavra, i, j);
 }
 
-void pSVH(){
+void pSVH(Lista *prim, char *palavra, int i, int j){
+    char word[30];
+    strcpy(word, palavra);
 
+    int k = 0, m = 0;
+    for(k = 0; k < i; k++){
+        for(m = 0; m < j; m++){
+
+        }
+    }
 }
 
 
