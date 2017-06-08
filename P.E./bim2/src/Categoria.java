b790ca5652504859b9c9db6706bbddd98e2a6ca7
patrickaraujo <araujo.patrick@live.com>
2017-06-08 15:55:08 -0300

    Create Categoria.java

---
+++ P.E./bim2/src/Categoria.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+/**
+ *
+ * @author Patrick
+ */
+public class Categoria {
+    float x;
+    float y;
+    
+    public Categoria(float y){
+        this.y = y/100;
+        this.x = (100-y)/100;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
