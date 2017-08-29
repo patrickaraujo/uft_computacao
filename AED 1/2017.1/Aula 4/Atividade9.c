82605c68aafc215ab1f0cb908832a68415040d6d
patrickaraujo <araujo.patrick@live.com>
2017-08-29 10:57:55 -0300

    Create Atividade9.c

---
+++ AED 1/2017.1/Aula 4/Atividade9.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+
+void funcao(int *m){
+    int i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 5; j++)
+            printf("%d\t", *m++);
+        printf("\n");
+    }
+}
+
+int main(){
+
+    system("clear");
+
+    int matriz[3][5];
+    int i, j;
+    for (i = 0; i < 3; i++)
+        for(j = 0; j < 5; j++)
+            matriz[i][j] = rand()%100;
+    funcao(&matriz);
+    return 0;
+}
