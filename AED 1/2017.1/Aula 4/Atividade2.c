2dbf4bad9e2d85eebba880cd14da0b0b7dcddcd0
patrickaraujo <araujo.patrick@live.com>
2017-08-29 08:42:45 -0300

    Create Atividade2.c

---
+++ AED 1/2017.1/Aula 4/Atividade2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void funcao(int x, int vetor[10]){
+    printf("Tamanho x = %d\n", sizeof(x));
+    printf("Tamanho vetor = %d\n\n" ,sizeof(vetor));
+}
+
+int main(){
+    int x = 5;
+    int vetor [10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    printf("Tamanho x = %d\n", sizeof(x));
+    printf("Tamanho vetor = %d\n\n", sizeof(vetor));
+    funcao(x, vetor);
+    return 0;
+}
