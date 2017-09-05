46ef6bd289ccbcd4d2d3ee4b80b8609ce1d5cc63
patrickaraujo <araujo.patrick@live.com>
2017-09-05 09:14:44 -0300

    Create remove.c

---
+++ AED 1/2017.1/Aula 5/remove.c
@@ -0,0 +1,44 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+# define TAM 100
+
+void imprimeVetor(char *vetor, int tam){
+    int i;
+    for(i = 0; i < tam; i++)
+        printf("vetor[%d] = %c\n", i, vetor[i]);
+}
+
+void preencheVetor(char *vetor, int tam){
+    srand(time(NULL));
+    int i;
+    for	(i = 0; i < tam; i++)
+        vetor[i] = (rand() % (122 - 97)) + 97;
+}
+
+void removeDado(char *vetor, int tam, char dado){
+    int i;
+    for (i = 0; i < tam; i++) {
+        if(vetor[i] == dado){
+            int j;
+            for(j = i; j < tam - 1; j++)
+                vetor[j] = vetor[j + 1];
+            vetor[tam - 1] = 0;
+            i--;
+        }
+    }
+}
+
+int main(){
+    system("clear");
+    char *vetor = malloc(sizeof(char) * TAM);
+    preencheVetor(vetor, TAM);
+    imprimeVetor(vetor, TAM);
+    char dado;
+    printf("Digite dado a ser removido: ");
+    scanf("%c", &dado);
+    removeDado(vetor, TAM, dado);
+    imprimeVetor(vetor, TAM);
+    free(vetor);
+    return 0;
+}
