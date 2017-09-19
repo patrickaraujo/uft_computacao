daf3dcf859834a0d06eef1ef7331ca9ab467d496
Patrick Araújo <araujo.patrick@icloud.com>
2017-09-19 11:32:25 -0300

    Update Palindromo.c

--- AED 1/2017.1/Aula 8/Palindromo.c
+++ AED 1/2017.1/Aula 8/Palindromo.c
@@ -3,21 +3,56 @@
 
 typedef struct no{
     char conteudo;
-    strict no *anterior;
-}point;
+    struct no *anterior;
+}ponto;
+
+void inserir(char *word, int tam){
+    int i;
+    for(i = 0; i < tam; i++)
+        push()
+}
 
 int main(){
-    
+    ponto * palavras = NULL;
+    printf("IDENTIFICACAO DE PALINDROMOS\n");
+    printf("Entre com a palavra\n");
+    char word[64];
+    fgets(word, sizeof(word), stdin);
+    int tam = strlen(&word)-1;
+    printf("%i", tam);
+    inserir(&word, tam);
+
     return 0;
 }
 
-void push (int num) {
+void push (ponto **Point, int num) {
     ponto *pont = malloc(sizeof(ponto));
     if(pont == NULL)
         printf("Memoria insulficiente\n");
     else{
-        pont->anterior = Point;
-        pont->x = num;
+//       pont->x = num;
+//       pont->anterior = *Point;
     }
-    Point = pont;
+//    *Point = pont;
+}
+
+void pop() {
+    ponto *aux;
+
+/*    if(Point == NULL)
+        printf("Nao ha elementos na pilha\n");
+    else{
+        aux = Point->prox;
+        free(Point);
+        // ver isso
+        Point = aux;
+    }
+
+}
+
+char top(no_char *p){
+    if(p!=NULL)
+        return p->conteudo;
+    return '\0';
+    */
 }
