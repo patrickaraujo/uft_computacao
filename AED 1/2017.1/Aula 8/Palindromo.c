0cb1c05dfaf61521a53ec5c08dc41f54a50f348c
Patrick Araújo <araujo.patrick@icloud.com>
2017-09-19 15:40:41 -0300

    Update Palindromo.c

--- AED 1/2017.1/Aula 8/Palindromo.c
+++ AED 1/2017.1/Aula 8/Palindromo.c
@@ -2,38 +2,41 @@
 #include <stdlib.h>
 
 typedef struct no{
-    char conteudo;
+    char letra;
     struct no *anterior;
 }ponto;
 
-void inserir(char *word, int tam){
+void push (ponto **Point, char letra);
+void imprimir(ponto *Point);
+
+void inserir(ponto **main, char *word, int tam){
     int i;
     for(i = 0; i < tam; i++)
-        push()
+        push(main, word[i]);
 }
 
 int main(){
-    ponto * palavras = NULL;
-    printf("IDENTIFICACAO DE PALINDROMOS\n");
-    printf("Entre com a palavra\n");
+    ponto *palavra = NULL;
+    printf("IDENTIFICACAO DE PALINDROMOS\n\n");
+    printf("Entre com a palavra: ");
     char word[64];
     fgets(word, sizeof(word), stdin);
     int tam = strlen(&word)-1;
-    printf("%i", tam);
-    inserir(&word, tam);
+    inserir(&palavra, &word, tam);
+    imprimir(palavra);
 
     return 0;
 }
 
-void push (ponto **Point, int num) {
+void push (ponto **Point, char letra) {
     ponto *pont = malloc(sizeof(ponto));
     if(pont == NULL)
         printf("Memoria insulficiente\n");
     else{
-//       pont->x = num;
-//       pont->anterior = *Point;
+       pont->anterior = *Point;
+       pont->letra = letra;
     }
-//    *Point = pont;
+    *Point = pont;
 }
 
 void pop() {
@@ -47,12 +50,20 @@ void pop() {
         // ver isso
         Point = aux;
     }
-
 }
-
 char top(no_char *p){
     if(p!=NULL)
         return p->conteudo;
     return '\0';
     */
+}
+
+void imprimir(ponto *Point){
+    ponto *aux;
+    if(Point != NULL)
+        for(aux = Point; aux != NULL; aux = aux->anterior){
+            printf("%c", aux->letra);
+        }
+    else
+        printf("Pilha vazia\n");
 }
