2294e969828723d559c1b7a26590b5e757efe968
Patrick Araújo <araujo.patrick@icloud.com>
2017-09-23 00:25:33 -0300

    Update TdHE.c

--- AED 1/2017.1/Trabalho - 3B/TdHE.c
+++ AED 1/2017.1/Trabalho - 3B/TdHE.c
@@ -1,7 +1,8 @@
 /**
-* @author Patrick Araújo: https://github.com/patrickaraujo
-* Trabalho Torre de Hanoi em C para a aula de Algoritmos e Estrutura de Dados 1
-* Finalizado em 22/09/2017
+* @author Patrick AraÃºjo: https://github.com/patrickaraujo
+* Trabalho Torre de Hanoi em C para a aula de Algoritmos e Estrutura de Dados 1 usando pilhas encadeadas
+* Assignment Tower of Hanoi in C language from the subject Algorithms and Data Structures 1 with Stacks
+* Finalizado em 22/09/2017/Concluded in 09/22/2017
 */
 
 #include <stdio.h>
@@ -83,32 +84,32 @@ void jogar(int tam){
             case 1:
                 if(compara(torre1, torre2))
                     if(!push(&torre2, pop(&torre1)))
-                        printf("Não foi possivel mover da Torre 1 para Torre 2");
+                        printf("NÃ£o foi possivel mover da Torre 1 para Torre 2");
             break;
             case 2:
                 if(compara(torre1, torre3))
                     if(!push(&torre3, pop(&torre1)))
-                        printf("Não foi possivel mover da Torre 1 para Torre 3");
+                        printf("NÃ£o foi possivel mover da Torre 1 para Torre 3");
             break;
             case 3:
                 if(compara(torre2, torre1))
                     if(!push(&torre1, pop(&torre2)))
-                        printf("Não foi possivel mover da Torre 2 para Torre 1");
+                        printf("NÃ£o foi possivel mover da Torre 2 para Torre 1");
             break;
             case 4:
                 if(compara(torre2, torre3))
                     if(!push(&torre3, pop(&torre2)))
-                        printf("Não foi possivel mover da Torre 2 para Torre 3");
+                        printf("NÃ£o foi possivel mover da Torre 2 para Torre 3");
             break;
             case 5:
                 if(compara(torre3, torre1))
                     if(!push(&torre1, pop(&torre3)))
-                        printf("Não foi possivel mover da Torre 3 para Torre 1");
+                        printf("NÃ£o foi possivel mover da Torre 3 para Torre 1");
             break;
             case 6:
                 if(compara(torre3, torre2))
                     if(!push(&torre2, pop(&torre3)))
-                        printf("Não foi possivel mover da Torre 3 para Torre 2");
+                        printf("NÃ£o foi possivel mover da Torre 3 para Torre 2");
             break;
             default:
                 printf("Invalido\n");
