7e1bbe757cd3af951e5648e8b0d05cc6a09b3256
Patrick Araújo <araujo.patrick@icloud.com>
2017-09-26 15:16:12 -0300

    Create FilaCircularSE.c

---
+++ AED 1/2017.1/Aula 9 - Fila/FilaCircularSE.c
@@ -0,0 +1,75 @@
+/**
+*   @author Patrick Araújo
+*   Fila Circular Sequencial Estática Circular em C para a aula de Algoritmos e Estrutura de Dados 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 10
+
+int inicio, fim;
+int fila[MAX];
+
+void push(int x){
+    if ( filaCheia() ){
+        fim = inicio; //  fila circular
+    }
+    printf("Inserindo: %i\n", x);
+    fila[fim++] = x;
+}
+
+int pop(){
+    int temp = fila[0];
+    if( temp ){
+        int i;
+        for(i = 0; fila[i+1] != NULL; i++)
+            fila[i] = fila[i+1];
+        fila[i] = NULL;
+        fim = i;
+        printf("Removido com sucesso\n");
+        return temp;
+    }
+    printf("Fila vazia\n");
+    return 0;
+}
+
+int filaCheia(){
+    return (fim == MAX);
+}
+
+void imprimeFila(){
+    int i;
+    printf("\nElementos do vetor:\n");
+    for(i = 0; i < MAX; i++){
+        printf("%d\n", fila[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    inicio = 0;
+    fim = 0;
+    int num, op;
+    do{
+        printf("Insercao de numeros\n1 - Para inserir\n2 - Para retirar\nO - Desistir\n\nOpcao: ");
+        scanf("%d", &op);
+        switch(op){
+            case 1:
+                printf("Numero: ");
+                scanf("%d", &num);
+                push(num);
+            break;
+            case 2:
+                pop();
+            break;
+            imprimeFila();
+            default:
+                printf("Opcao invalida");
+        }
+        imprimeFila();
+        printf("fim: %i\n", fim);
+    }while(op);
+    return 0;
+}
