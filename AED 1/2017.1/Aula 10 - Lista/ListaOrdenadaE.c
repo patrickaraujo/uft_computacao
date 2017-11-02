08a481444bb3831ea608fa7120c2e5ea0bbcb5c0
Patrick Araújo <araujo.patrick@icloud.com>
2017-11-02 20:35:26 -0300

    Update ListaOrdenadaE.c

--- AED 1/2017.1/Aula 10 - Lista/ListaOrdenadaE.c
+++ AED 1/2017.1/Aula 10 - Lista/ListaOrdenadaE.c
@@ -13,18 +13,22 @@ typedef struct reg {
 
 void imprimir(Lista *main);
 Lista* cria_lista();
-int insere_lista_ordenada(Lista **li, int num);
+int insere_lista_ordenada(Lista **main, int num);
+int remove_lista(Lista **li, int num);
+Lista* find(Lista *main, int valor);
+int tamanho_lista(Lista *main);
 
 int main(){
-    Lista* li = NULL;
+    Lista* main = NULL;
     int choice, x;
 
-    while(choice != 4){
+    while(choice != 5){
         printf("\n\tMENU\t\n\n");
-        printf("1 - Para Empilhar(PUSH)\n");
-        printf("2 - Para Desempilhar(POP)\n");
-        printf("3 - Mostrar elementos da pilha\n");
-        printf("4 - Para Sair\n");
+        printf("1 - Para inserir numero\n");
+        printf("2 - Para remover numero\n");
+        printf("3 - Mostrar elementos armazenados\n");
+        printf("4 - Achar indice de um numero\n");
+        printf("5 - Para Sair\n");
         printf("\n\n");
         printf("Informe A Opcao: ");
         scanf("%d", &choice);
@@ -33,13 +37,22 @@ int main(){
             case 1:
                 printf("Digite o numero a inserir\n");
                 scanf("%d", &x);
-                insere_lista_ordenada(&li, x);
+                insere_lista_ordenada(&main, x);
             break;
             case 2:
-                //  dequeue(&prim);
+                printf("Digite o numero a remover\n");
+                scanf("%d", &x);
+                remove_lista(&main, x);
             break;
             case 3:
-                imprimir(li);
+                imprimir(main);
+                printf("\n\nQuantidade: %i\n", tamanho_lista(main));
+                system("pause");
+            break;
+            case 4:
+                printf("Digite o numero a ser achado\n");
+                scanf("%d", &x);
+                printf("Indice: %x", find(main, x));
                 system("pause");
             break;
             default:
@@ -55,14 +68,9 @@ int insere_lista_ordenada(Lista **main, int num){
     Lista *aux = malloc(sizeof(Lista));
     if(aux){
         aux->info = num;
-        if((*main) == NULL){//lista vazia: insere início
-            aux->prox = NULL;
-            *main = aux;
-            return 1;
-        }
-        else{
+        if(*main){
             Lista *ant, *atual = *main;
-            while(atual != NULL && atual->info < num){
+            while(atual && atual->info < num){
                 ant = atual;
                 atual = atual->prox;
             }
@@ -76,9 +84,45 @@ int insere_lista_ordenada(Lista **main, int num){
             }
             return 1;
         }
+        else{
+            aux->prox = NULL;
+            *main = aux;
+            return 1;
+        }
     }
     return 0;
 }
+
+int remove_lista(Lista **main, int num){
+    if(*main){
+        Lista *ant, *aux = *main;
+        while(aux && aux->info != num){
+            ant = aux;
+            aux = aux->prox;
+        }
+        if(!aux)//não encontrado
+            return 0;
+        if(aux == *main)//remover o primeiro?
+            *main = aux->prox;
+        else
+            ant->prox = aux->prox;
+        free(aux);
+        return 1;
+    }
+    return 0;
+}
+
+Lista* find(Lista *main, int valor){
+	Lista *retorno = main;
+	if(main)
+        while(retorno){
+            if((retorno->info) == valor)
+                return retorno;
+            retorno = retorno->prox;
+        }
+    return NULL;
+}
+
 
 void imprimir(Lista *main){
     Lista *aux = main;
@@ -89,4 +133,17 @@ void imprimir(Lista *main){
 			printf("\nElemento: %d\n", aux->info);
 			aux = aux->prox;
 		}
+}
+
+int tamanho_lista(Lista *main){
+    if(main){
+        int cont = 0;
+        Lista *aux = main;
+        while(aux){
+            cont++;
+            aux = aux->prox;
+        }
+        return cont;
+    }
+    return NULL;
 }
