2f25ccdfbd9461bb4de9e8ccaa1a008a099c32ca
Patrick Araújo <araujo.patrick@icloud.com>
2017-11-04 20:08:11 -0300

    Update ListaE.c

--- AED 1/2017.1/Aula 10 - Lista/ListaE.c
+++ AED 1/2017.1/Aula 10 - Lista/ListaE.c
@@ -1,260 +1,136 @@
 /**
 *   @author Patrick Araújo: https://github.com/patrickaraujo
-*   Fila Encadeada em C para a aula de Algoritmos e Estrutura de Dados 1
+*   Lista Encadeada em C para a aula de Algoritmos e Estrutura de Dados 1
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct aluno{
-    int matricula;
-    char nome[30];
-    float n1,n2,n3;
-};
+typedef struct reg {
+    int info;
+    struct reg *prox;
+}Lista;
 
-typedef struct elemento* Lista;
-
-struct elemento{
-    struct aluno dados;
-    struct elemento *prox;
-};
-typedef struct elemento Elem;
-
-Lista* cria_lista(){
-    Lista* li = (Lista*) malloc(sizeof(Lista));
-    if(li != NULL)
-        *li = NULL;
-    return li;
-}
-
-void libera_lista(Lista* li){
-    if(li != NULL){
-        Elem* no;
-        while((*li) != NULL){
-            no = *li;
-            *li = (*li)->prox;
-            free(no);
-        }
-        free(li);
-    }
-}
-
-int consulta_lista_pos(Lista* li, int pos, struct aluno *al){
-    if(li == NULL || pos <= 0)
-        return 0;
-    Elem *no = *li;
-    int i = 1;
-    while(no != NULL && i < pos){
-        no = no->prox;
-        i++;
-    }
-    if(no == NULL)
-        return 0;
-    else{
-        *al = no->dados;
-        return 1;
-    }
-}
-
-int consulta_lista_mat(Lista* li, int mat, struct aluno *al){
-    if(li == NULL)
-        return 0;
-    Elem *no = *li;
-    while(no != NULL && no->dados.matricula != mat){
-        no = no->prox;
-    }
-    if(no == NULL)
-        return 0;
-    else{
-        *al = no->dados;
-        return 1;
-    }
-}
-
-int insere_lista_final(Lista* li, struct aluno al){
-    if(li == NULL)
-        return 0;
-    Elem *no;
-    no = (Elem*) malloc(sizeof(Elem));
-    if(no == NULL)
-        return 0;
-    no->dados = al;
-    no->prox = NULL;
-    if((*li) == NULL){//lista vazia: insere início
-        *li = no;
-    }else{
-        Elem *aux;
-        aux = *li;
-        while(aux->prox != NULL){
-            aux = aux->prox;
-        }
-        aux->prox = no;
-    }
-    return 1;
-}
-
-int insere_lista_inicio(Lista* li, struct aluno al){
-    if(li == NULL)
-        return 0;
-    Elem* no;
-    no = (Elem*) malloc(sizeof(Elem));
-    if(no == NULL)
-        return 0;
-    no->dados = al;
-    no->prox = (*li);
-    *li = no;
-    return 1;
-}
-
-int insere_lista_ordenada(Lista* li, struct aluno al){
-    if(li == NULL)
-        return 0;
-    Elem *no = (Elem*) malloc(sizeof(Elem));
-    if(no == NULL)
-        return 0;
-    no->dados = al;
-    if((*li) == NULL){//lista vazia: insere início
-        no->prox = NULL;
-        *li = no;
-        return 1;
-    }
-    else{
-        Elem *ant, *atual = *li;
-        while(atual != NULL && atual->dados.matricula < al.matricula){
-            ant = atual;
-            atual = atual->prox;
-        }
-        if(atual == *li){//insere início
-            no->prox = (*li);
-            *li = no;
-        }else{
-            no->prox = atual;
-            ant->prox = no;
-        }
-        return 1;
-    }
-}
-
-int remove_lista(Lista* li, int mat){
-    if(li == NULL)
-        return 0;
-    if((*li) == NULL)//lista vazia
-        return 0;
-    Elem *ant, *no = *li;
-    while(no != NULL && no->dados.matricula != mat){
-        ant = no;
-        no = no->prox;
-    }
-    if(no == NULL)//não encontrado
-        return 0;
-
-    if(no == *li)//remover o primeiro?
-        *li = no->prox;
-    else
-        ant->prox = no->prox;
-    free(no);
-    return 1;
-}
-
-int remove_lista_inicio(Lista* li){
-    if(li == NULL)
-        return 0;
-    if((*li) == NULL)//lista vazia
-        return 0;
-
-    Elem *no = *li;
-    *li = no->prox;
-    free(no);
-    return 1;
-}
-
-int remove_lista_final(Lista* li){
-    if(li == NULL)
-        return 0;
-    if((*li) == NULL)//lista vazia
-        return 0;
-
-    Elem *ant, *no = *li;
-    while(no->prox != NULL){
-        ant = no;
-        no = no->prox;
-    }
-
-    if(no == (*li))//remover o primeiro?
-        *li = no->prox;
-    else
-        ant->prox = no->prox;
-    free(no);
-    return 1;
-}
-
-int tamanho_lista(Lista* li){
-    if(li == NULL)
-        return 0;
-    int cont = 0;
-    Elem* no = *li;
-    while(no != NULL){
-        cont++;
-        no = no->prox;
-    }
-    return cont;
-}
-
-int lista_cheia(Lista* li){
-    return 0;
-}
-
-int lista_vazia(Lista* li){
-    if(li == NULL)
-        return 1;
-    if(*li == NULL)
-        return 1;
-    return 0;
-}
-
-void imprime_lista(Lista* li){
-    if(li == NULL)
-        return;
-    Elem* no = *li;
-    while(no != NULL){
-        printf("Matricula: %d\n",no->dados.matricula);
-        printf("Nome: %s\n",no->dados.nome);
-        printf("Notas: %f %f %f\n",no->dados.n1,
-                                   no->dados.n2,
-                                   no->dados.n3);
-        printf("-------------------------------\n");
-
-        no = no->prox;
-    }
-}
-
+int insere_lista_inicio (Lista **main, int x);
+int remove_lista_final(Lista **main);
+void imprimir(Lista *main);
+int lista_vazia(Lista *main);
+Lista* find(Lista *main, int valor);
+void libera_lista(Lista **main);
+int tamanho_lista(Lista *main);
 
 int main(){
-    struct aluno al, a[4] = {{2,"Andre",9.5,7.8,8.5},
-                         {4,"Ricardo",7.5,8.7,6.8},
-                         {1,"Bianca",9.7,6.7,8.4},
-                         {3,"Ana",5.7,6.1,7.4}};
-    Lista* li = cria_lista();
-    printf("Tamanho: %d\n\n\n\n",tamanho_lista(li));
+    Lista *main = NULL;
+    int choice, x;
 
-    int i;
-    for(i=0; i < 4; i++)
-        insere_lista_ordenada(li,a[i]);
+    while(choice != 4){
+        printf("\n\tMENU\t\n\n");
+        printf("1 - Para inserir elemento no topo\n");
+        printf("2 - Para remover final\n");
+        printf("3 - Mostrar todos elementos\n");
+        printf("4 - Para Sair\n");
+        printf("\n\n");
+        printf("Informe A Opcao: ");
+        scanf("%d", &choice);
 
-    imprime_lista(li);
-    printf("\n\n\n\n Tamanho: %d\n",tamanho_lista(li));
-
-    for(i=0; i < 4; i++){
-        remove_lista_final(li);
-        imprime_lista(li);
-        printf("\n Tamanho: %d\n\n\n",tamanho_lista(li));
+        switch(choice) {
+            case 1:
+                printf("Digite o numero a inserir\n");
+                scanf("%d", &x);
+                insere_lista_inicio(&main, x);
+            break;
+            case 2:
+                remove_lista_final(&main);
+            break;
+            case 3:
+                imprimir(main);
+                system("pause");
+            break;
+            default:
+                printf("\nOpcao invalida");
+        }
+        system("cls");
     }
-
-    for(i=0; i < 4; i++)
-        insere_lista_ordenada(li,a[i]);
-    imprime_lista(li);
-
-    libera_lista(li);
     system("pause");
     return 0;
+}
+
+int insere_lista_inicio(Lista **main, int x){
+    Lista *aux = malloc(sizeof(Lista));
+    if(aux){
+        aux->info = x;
+        aux->prox = (*main);
+        *main = aux;
+        return 1;
+    }
+    return NULL;
+}
+
+int remove_lista_final(Lista **main){
+    if(*main){
+        Lista *ant, *aux = (*main);
+        while(aux->prox){
+            ant = aux;
+            aux = aux->prox;
+        }
+        int retorno = aux->info;
+        if(aux == (*main))  //  remover o primeiro?
+            *main = aux->prox;
+        else
+            ant->prox = aux->prox;
+        free(aux);
+        return retorno;
+    }
+    return NULL;
+}
+
+int lista_vazia(Lista *main){
+    if(main)
+        return 1;
+    return NULL;
+}
+
+void libera_lista(Lista **main){
+    Lista *aux;
+    while(*main){
+        aux = *main;
+        *main = (*main)->prox;
+        free(aux);
+    }
+    free(*main);
+}
+
+Lista* find(Lista *main, int valor){
+	Lista *retorno = main;
+	if(main)
+        while(retorno){
+            if((retorno->info) == valor)
+                return retorno;
+            retorno = retorno->prox;
+        }
+    return NULL;
+}
+
+int tamanho_lista(Lista *main){
+    if(main){
+        int cont = 0;
+        Lista *aux = main;
+        while(aux){
+            cont++;
+            aux = aux->prox;
+        }
+        return cont;
+    }
+    return NULL;
+}
+
+void imprimir(Lista *main){
+    Lista *aux = main;
+	if(!main)
+		printf("\nLista Vazia\n");
+	else
+		while(aux){
+			printf("\nElemento: %d\n", aux->info);
+			aux = aux->prox;
+		}
 }
