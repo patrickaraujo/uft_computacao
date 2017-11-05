ce0e62a163c4e513cbf28dee8244970522e44de5
Patrick Araújo <araujo.patrick@icloud.com>
2017-11-05 12:40:59 -0300

    Update FilaDuplamenteE.c

--- AED 1/2017.1/Aula 9 - Fila/FilaDuplamenteE.c
+++ AED 1/2017.1/Aula 9 - Fila/FilaDuplamenteE.c
@@ -1,6 +1,6 @@
 /**
 *   @author Patrick Araújo: https://github.com/patrickaraujo
-*   Fila Encadeada em C para a aula de Algoritmos e Estrutura de Dados 1
+*   Fila Duplamente Encadeada em C para a aula de Algoritmos e Estrutura de Dados 1
 */
 
 #include <stdio.h>
