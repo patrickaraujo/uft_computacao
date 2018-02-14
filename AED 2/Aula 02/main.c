27a87fcda3f9a95c8ae9022853d2ad8eab856bc1
Patrick Araújo <araujo.patrick@icloud.com>
2018-02-14 17:07:22 -0300

    Update main.c

--- AED 2/Aula 02/main.c
+++ AED 2/Aula 02/main.c
@@ -1,8 +1,26 @@
+/**
+*   @author Patrick Araújo: https://github.com/patrickaraujo
+*   https://gist.github.com/marcoscastro/3e4abc13dec4eddb7894
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "TBT.h"
 
 int main(){
-    printf("Hello world!\n");
+    TBT *main = NULL;
+    insereR(&main, 12);
+    insereR(&main, 17);
+    insereR(&main, 2);
+    insereR(&main, 7);
+    insereR(&main, 9);
+    insereR(&main, 20);
+    print(main);
+    int p = 0;
+    printf("\nAltura: %i\n", alturaR(main));
+    if(busca(main, p))
+        printf("%i esta na arvore", p);
+    else
+        printf("%i nao esta na arvore", p);
     return 0;
 }
