b14c2ecd21beaf7a0470ef80fbffc65708b1c8da
Patrick Araújo <araujo.patrick@icloud.com>
2018-02-20 18:18:20 -0300

    Update main.c

--- AED 2/Aula 02/main.c
+++ AED 2/Aula 02/main.c
@@ -17,10 +17,26 @@ int main(){
     insereR(&main, 20);
     print(main);
     int p = 0;
-    printf("\nAltura: %i\n", alturaR(main));
+    printf("\nAltura: %i\n", altura(main));
+    printf("\nContar folhas: %i\n", contarFolhas(main));
     if(busca(main, p))
         printf("%i esta na arvore", p);
     else
         printf("%i nao esta na arvore", p);
+
+    printf("\n\nPercurso em pre-ordem\n");
+    exibirPreOrdem(main);
+    printf("\n\nPercurso em ordem simetrica\n");
+    exibirEmOrdem(main);
+    printf("\n\nPercurso em pos-ordem\n");
+    exibirPosOrdem(main);
+    printf("\n\nEsta cheia?:\t");
+    if(cheia(main))
+        printf("Sim!");
+    else
+        printf("Nao");
+    printf("\n\nEspelho:\t");
+    espelho(main);
+    print(main);
     return 0;
 }
