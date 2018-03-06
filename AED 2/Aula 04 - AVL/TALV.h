64d31cb469ff10dd1816d2e27a8ecaa7046f211b
Patrick Araújo <araujo.patrick@icloud.com>
2018-03-06 19:43:00 -0300

    Update TALV.h

--- AED 2/Aula 04 - AVL/TALV.h
+++ AED 2/Aula 04 - AVL/TALV.h
@@ -44,7 +44,6 @@ int altura_NO(TAVL *main){
 }
 
 void RotacaoLL(TAVL **A){	//	LL
-    printf("RotacaoLL\n");
     TAVL *B = (*A)->esq;
     (*A)->esq = B->dir;
     B->dir = (*A);
@@ -54,7 +53,6 @@ void RotacaoLL(TAVL **A){	//	LL
 }
 
 void RotacaoRR(TAVL **A){	//	RR
-    printf("RotacaoRR\n");
     TAVL *B = (*A)->dir;
     (*A)->dir = B->esq;
     B->esq = (*A);
@@ -76,7 +74,6 @@ void RotacaoRL(TAVL **A){//RL
 int insere_ArvAVL(TAVL **main, int valor){
     int res;
     if(!(*main)){   //  árvore vazia ou nó folha
-            printf("bingo");
         TAVL *novo = (TAVL *) malloc(sizeof(TAVL));
         novo->info = valor;
         novo->fb = 0;
@@ -97,21 +94,86 @@ int insere_ArvAVL(TAVL **main, int valor){
     }
     else{
         //  if(valor > atual->info)
-            if((res = insere_ArvAVL((&(atual->dir)), valor)) == 1)
-                if(fatorBalanceamento_NO(atual) >= 2)
-                    if((*main)->dir->info < valor)
-                        RotacaoRR(main);
-                    else
-                        RotacaoRL(main);
-        /*else
-            printf("Valor duplicado!!\n");
+        if((res = insere_ArvAVL((&(atual->dir)), valor)) == 1)
+            if(fatorBalanceamento_NO(atual) >= 2)
+                if((*main)->dir->info < valor)
+                    RotacaoRR(main);
+                else
+                    RotacaoRL(main);
+        /*
+            else
+                printf("Valor duplicado!!\n");
             return NULL;
-            */
+        */
     }
 
     atual->fb = maior(altura_NO(atual->esq),altura_NO(atual->dir)) + 1;
 
     return res;
+}
+
+TAVL* procuraMenor(TAVL* atual){
+    TAVL *no1 = atual, *no2 = atual->esq;
+    while(no2 != NULL){
+        no1 = no2;
+        no2 = no2->esq;
+    }
+    return no1;
+}
+
+int remove_ArvAVL(TAVL **main, int valor){
+	if(*main == NULL){// valor não existe
+	    printf("valor não existe!!\n");
+	    return 0;
+	}
+
+    int res;
+	if(valor < (*main)->info)
+	    if((res = remove_ArvAVL(&(*main)->esq,valor)) == 1)
+            if(fatorBalanceamento_NO(*main) >= 2){
+                if(altura_NO((*main)->dir->esq) <= altura_NO((*main)->dir->dir))
+                    RotacaoRR(main);
+                else
+                    RotacaoRL(main);
+            }
+    //  if((*main)->info < valor)
+	else
+	    if((res = remove_ArvAVL(&(*main)->dir, valor)) == 1)
+            if(fatorBalanceamento_NO(*main) >= 2){
+                if(altura_NO((*main)->esq->dir) <= altura_NO((*main)->esq->esq) )
+                    RotacaoLL(main);
+                else
+                    RotacaoLR(main);
+            }
+
+	if((*main)->info == valor){
+	    if(((*main)->esq == NULL || (*main)->dir == NULL)){ //  nó tem 1 filho ou nenhum
+			TAVL *oldNode = (*main);
+			if((*main)->esq != NULL)
+                *main = (*main)->esq;
+            else
+                *main = (*main)->dir;
+			free(oldNode);
+		}
+		else {  //  nó tem 2 filhos
+			TAVL *temp = procuraMenor((*main)->dir);
+			(*main)->info = temp->info;
+			remove_ArvAVL(&(*main)->dir, (*main)->info);
+            if(fatorBalanceamento_NO(*main) >= 2){
+				if(altura_NO((*main)->esq->dir) <= altura_NO((*main)->esq->esq))
+					RotacaoLL(main);
+				else
+					RotacaoLR(main);
+			}
+		}
+		if (*main)
+            (*main)->fb = maior(altura_NO((*main)->esq),altura_NO((*main)->dir)) + 1;
+		return 1;
+	}
+
+	(*main)->fb = maior(altura_NO((*main)->esq),altura_NO((*main)->dir)) + 1;
+
+	return res;
 }
 
 #endif // TAVL_H_INCLUDED
